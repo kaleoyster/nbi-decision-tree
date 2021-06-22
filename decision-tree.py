c8c80dee0beb4f1bcee3a2e46b1f362a2035fe08
Akshay Kale <akshaykale915@gmail.com>
2021-06-22 11:19:40 -0500

    dangling updates

--- decision-tree.py
+++ decision-tree.py
@@ -1,5 +1,14 @@
 """
-Decision - tree with a selction of replacements, rehabs, and redecks
+Decision - flow chart for identification of replacements, rehabs, and redecks.
+If already in github remove it from there.
+
+Note:
+    - There may be some of the deadzones
+    - Cover the deadzones in the driver program
+    - Re-evalauate the decision tree for correctness
+    - Change the name of the files
+    - Compute the list of bridges that can be compared against the maintained list:
+        - Run the program for the year 2016, 2017, 2018, and 2019
 """
 import csv
 import pandas as pd
@@ -9,12 +18,13 @@ import io
 class Flow():
     def __init__(self):
         """
-          Contains a set of conditions
+        Notes:
+          - Contains a set of conditions
         """
 
     def condition_1_1(self, df):
-        """ 
-
+        """
+        Notes:
         """
         if df['Pile Condition'] < 3:
             return True
@@ -22,6 +32,9 @@ class Flow():
             return False
 
     def condition_2_1(self, df):
+        """
+        Notes:
+        """
         if condition1_1(self, df) == False:
             if df['Timber Pile'] == T:
                 return True
@@ -31,16 +44,21 @@ class Flow():
             return False
 
     def condition_3_1(self, df):
-        "Returns a pandas dataframe that satisifies the condition 3.1, returns True if the bridge qualifies for replacement  "
-     #   if df['SUBSTRUCTURE_COND_060'] < 3:
-     #       return True
-     #   else:
-     #       return False
+        """
+        Description:
+         - Returns a pandas dataframe that satisifies the condition 3.1,
+        returns True if the bridge qualifies for replacement
+
+        Notes:
+        """
         df = df[~df['SUBSTRUCTURE_COND_060'].isin(['N'])]
-        df['SUBSTRUCTURE_COND_060'] = df['SUBSTRUCTURE_COND_060'].astype(int)
+        df['SUBSTRUCTURE_COND_060'] = df['SUBSTRUCTURE_COND_060'].dropna().astype(int)
         return df[df['SUBSTRUCTURE_COND_060'] < 3]
 
     def condition_4_1(self, df):
+        """
+        Notes:
+        """
         if condition_1_1(self, df) and condition_2_1(self, df) and condition_3_1(self, df) == False:
             if df['Scour Critical'] < 3:
                 return True
@@ -50,6 +68,9 @@ class Flow():
             return False
 
     def condition_4_2(self, df):
+        """
+        Notes:
+        """
         if condition_1_1(self, df) and condition_2_1(self, df) and condition_3_1(self, df) == False:
             if df['Scour Critical']  == 8:
                 return True
@@ -59,27 +80,42 @@ class Flow():
             return False
 
     def HydrologyReviewMitigateScour(self, df):
+        """
+        Notes:
+        """
         pass
 
     def condition_5_1(self, df):
+        """
+        Notes:
+        """
         if HydrologyReviewMitigateScour(self, df) == True:
             return True
         else:
             return False
 
     def condition_5_2(self, df):
+        """
+        Notes:
+        """
         if HydrologyReviewMitigateScour(self, df) == False:
             return True
         else:
             return False
 
     def condition_6_1(self, df):
+        """
+        Notes:
+        """
         if df['Age'] > 75:
             return True
         else:
             return False
 
     def condition_7(self, df):
+        """
+        Notes:
+        """
         if condition_1_1(self, df) and condition_2_1(self, df) and condition_3_1(self, df) and condition_6_1(self, df) == False:
             if df['Age'] <= 75:
                 return True
@@ -87,42 +123,62 @@ class Flow():
                 return False
 
     def repairSubstructureFeasibile(self, df):
+        """
+        Notes:
+        """
         pass
 
     def condition_8(self, df):
+        """
+        Notes:
+        """
         if repairSubstructureFeasible(self, df) == False:
             return True
         else:
             return False
 
     def condition_8_1(self, df):
+        """
+        Notes:
+        """
         if repairSubstructureFeasible(self, df) == True:
             return True
         else:
             return False
 
     def condition_9_1(self, df):
-        if p&h > 0: 
+        """
+        Notes:
+        """
+        if p&h > 0:
             return True
         else:
             return False
 
     def condition_9_2(self, df):
+        """
+        Notes:
+        """
         if frac_crtit != 0:
             return True
         else:
             return False
 
-    # There may be some of the deadzones
-    # Cover the deadzones in the driver program
-
     def condition_10_1(self, df):
+        """
+        Notes:
+            -
+        """
         if df['Age'] > 75:
             return True
         else:
             return False
 
     def condition_11_1(self, df):
+        """
+        Notes:
+            -
+        """
         if df['Age'] < 75:
             if df['Design Load'] >= HS15:
                 return True
@@ -130,8 +186,11 @@ class Flow():
                 return False
 
     def condition_12_1(self, df):
-        ## 12.1 occurs only if 1.1, 2.1, 3.1, 6.1, 10.1 = OK
-        ## 4.1, or 4.2, and 7.0
+        """
+        Notes:
+            - 12.1 occurs only if 1.1, 2.1, 3.1, 6.1, 10.1 = OK
+            - 4.1, or 4.2, and 7.0
+        """
         if df['Age'] < 75:
             if df['Design Load'] < HS15:
                 return True
@@ -139,87 +198,74 @@ class Flow():
                 return False
 
     def condition_13_1(self, df):
+        """
+        Notes:
+            -
+        """
         if rehabPendingSubCapacityReview == False:
             return True
         else:
             return False
 
     def condition_13_2(self, df):
+        """
+        Notes:
+            -
+        """
         if rehabPendingSubCapacityReview == True:
             return True
         else:
             return False
 
-#    def condition_16_2(self, df):
-#        if condition_1_1(self, df) and condition_2_1(self, df) and condition_3_1(self, df) and condition_6_1(self, df) and condition_9_1(self, df) and condition_9_2(self, df) and condition_14_1(self, df) and condition_15_1(self, df) and condition_4_1(self, df) and condition_4_2(self, df) and condition_7_0(self, df) == False:
-#           if df['deck'] > 5:
-#                return True
-#           else:
-#                return False
-#        else:
-#            False
-#    
-#    def condition_14_1(self, df):
-#        if condition_1_1(self, df) and condition_2_1(self, df) and condition_3_1(self, df)and condition_6_1(self, df) and condition_9_1(self, df)  and condition_9_2(self, df) == False:
-#            if df['Deck'] < 4:
-#                  return True
-#              else:
-#                  return False
-#        else:
-#            False
-#      
-#    def condition_15_1(self, df):
-#        if condition_1_1(self, df) and condition_2_1(self, df)  and condition_3_1(self, df) and condition_6_1(self, df) and condition_9_1(self, df) and condition_9_2(self, df) and condition_14_1(self, df) or condition_14_2(self, df) and condition_7_0(self, df) == False:
-#              if df['Asphalt 108'] != 6:
-#                  return True
-#              else:
-#                  return False
-#          else:
-#              False
-#
-#    def condition_16_1(self, df):
-#        if condition_1_1(self, df) and condition_2_1(self, df) and condition_3_1(self, df) and condition_6_1(self, df) and condition_9_1(self, df) and condition_9_2(self, df) and condition_14_1(self, df) and condition_15_1(self, df) and condition_4_1(self, df) and condition_4_2(self, df) and condition_7_0(self, df) == False:
-#                if df['Asphalt'] ==  6:
-#                    return True
-#                else:
-#                    return False
-#            else:
-#                False
-#
-#def main():
-#    maintain = Flow()
-#    
-#    # data path
-#    data_path = 'ne18.csv'
-#    
-#    # Creating DataFrame 
-#    df = pd.read_csv(data_path)
-#    
-#    # Change the column names of the dataframe
-#    
-#    # filter data that qualify for condition 3_1
-#    df_3_1 = maintain.condition_3_1(df) 
-#    print(df_3_1)
-#    
-#
-#if  __name__ == '__main__':
-#    main()
+    def condition_16_2(self, df):
+        if condition_1_1(self, df) and condition_2_1(self, df) and condition_3_1(self, df) and condition_6_1(self, df) and condition_9_1(self, df) and condition_9_2(self, df) and condition_14_1(self, df) and condition_15_1(self, df) and condition_4_1(self, df) and condition_4_2(self, df) and condition_7_0(self, df) == False:
+           if df['deck'] > 5:
+                return True
+           else:
+                return False
+        else:
+            False
 
+    def condition_14_1(self, df):
+        if condition_1_1(self, df) and condition_2_1(self, df) and condition_3_1(self, df)and condition_6_1(self, df) and condition_9_1(self, df)  and condition_9_2(self, df) == False:
+            if df['Deck'] < 4:
+                  return True
+            else:
+                  return False
+        else:
+            False
 
+    def condition_15_1(self, df):
+        if condition_1_1(self, df) and condition_2_1(self, df)  and condition_3_1(self, df) and condition_6_1(self, df) and condition_9_1(self, df) and condition_9_2(self, df) and condition_14_1(self, df) or condition_14_2(self, df) and condition_7_0(self, df) == False:
+              if df['Asphalt 108'] != 6:
+                  return True
+              else:
+                  return False
+        else:
+            False
 
+    def condition_16_1(self, df):
+        if condition_1_1(self, df) and condition_2_1(self, df) and condition_3_1(self, df) and condition_6_1(self, df) and condition_9_1(self, df) and condition_9_2(self, df) and condition_14_1(self, df) and condition_15_1(self, df) and condition_4_1(self, df) and condition_4_2(self, df) and condition_7_0(self, df) == False:
+                if df['Asphalt'] ==  6:
+                    return True
+                else:
+                    return False
+        else:
+            False
 
+def main():
+    maintain = Flow()
 
+    # data path
+    data_path = 'ne-18.csv'
 
+    # Creating DataFrame 
+    df = pd.read_csv(data_path)
 
+    # Change the column names of the dataframe
+    # filter data that qualify for condition 3_1
+    df_3_1 = maintain.condition_3_1(df)
+    print(df_3_1)
 
-
-
-
-
-
-        
-
-
-
-
-
+if  __name__ == '__main__':
+    main()
--- flow.py
+++ flow.py
@@ -2,34 +2,35 @@
 title: This file contains functions that emulate the conditions of
 deicison flow chart by NDOT
 author: Akshay Kale
+
+Notes:
+     - TODO:
+        Preprocess (Pipeline):
+          1. (List) Extract all the necessary attribute
+          2. Using if and else, create a pipeline, that takes in:
+                      1. Args: (record, datastructure)
+                      2. Returns: report (JSON datastructure)
+          3. Run decision flow chart -> Create condition Report
+
+        Datastructure ( Report ):
+          1. {structure: {condition1: True,
+                           condition2: False,
+                           condition3: True,
+                           condition4: False,
+                           condition5: False
+             }}
+
+        Create Decision Flow Chart:
+          1. For every record -> pipeline (preprocess) -> Report
+          2. preprocess
 """
 import csv
+from collections import defaultdict
 
 __author__ = 'Akshay Kale'
 __copyright__ = 'GPL'
 __credit__ = []
 __email__ = 'akale@unomaha.edu'
-
-#TODO
-
-# Preprocess (Pipeline): 
-#   1. (List) Extract all the necessary attribute
-#   2. Using if and else, create a pipeline, that takes in:
-#               1. Args: (record, datastructure)
-#               2. Returns: report (JSON datastructure)
-#   3. Run decision flow chart -> Create condition Report
-
-# Datastructure ( Report ):
-#   1. {structure: {condition1: True,
-#                    condition2: False,
-#                    condition3: True,
-#                    condition4: False,
-#                    condition5: False
-#      }}
-
-# Create Decision Flow Chart:
-#   1. For every record -> pipeline (preprocess) -> Report
-#   2. preprocess
 
 
 def condition1_1(records):
@@ -49,7 +50,7 @@ def condition1_1(records):
     return structures
 
 
-def decision_flow_chart(csvReader):
+def decision_flow_chart(listOfFields):
     """
     Description: takes in filename and performs conditional checks
     Args:
@@ -58,19 +59,35 @@ def decision_flow_chart(csvReader):
         structures (list): a list of structures that require maintenance
     """
     report = list()
-    for record in csvReader:
+    for record in listOfFields:
         # Extract fields
-        # Execute function with filed
-        fieldStat1_1 = condition1_1(field)
+        # execute function with field
+        print(record)
+        break
+        #fieldStat1_1 = condition1_1(field)
 
-        field1 = record[0]
-        if condition1_1(fieldStat1_1) is True:
-            report.append(True)
-        elif condition1_2(field1) is True:
-            report.append(False)
-        else:
-            report.append(None)
+        #field1 = record[0]
+        #if condition1_1(fieldStat1_1) is True:
+        #    report.append(True)
+        #elif condition1_2(field1) is True:
+        #    report.append(False)
+        #else:
+        #    report.append(None)
     return report
+
+def get_state_data(listOfAllField, state='31'):
+    """
+    Description: Get nebraska data.
+    Args:
+        - listOfAllFields: Inspection records of all states.
+        - state: 'NE'.
+    Return: data (list): list of inspection records from the state.
+    """
+    data = list()
+    for record in listOfAllField:
+        if record['State Code'] == state:
+            data.append(record.values())
+    return data
 
 
 def read_csv(filename):
@@ -81,19 +98,26 @@ def read_csv(filename):
     Returns:
         structures (list): a list of structures that require maintenance
     """
+    listOfFields = list()
     with open(filename, 'r') as csvFile:
         csvReader = csv.reader(csvFile, delimiter=',')
         header = next(csvReader)
-    return header
+        recordDict = defaultdict()
+        for row in csvReader:
+            recordDict = {head:val for head, val in zip(header[1:], row[1:])}
+            listOfFields.append(recordDict)
+    return header, listOfFields
 
 
 def main():
     directory = '../data/nbi/'
     # need the raw nbi file
-    csvFileName = '06-20-19-thesis-dataset_allstates_allstates.csv'
-
+    csvFileName = '06-20-19-thesis-dataset-without-outliers_allstates.csv'
     filename = directory + csvFileName
-    print(decision_flow_chart(filename))
+    header, listOfFields = read_csv(csvFileName)
+    stateData = get_state_data(listOfFields)
+    print(stateData)
+    #decision_flow_chart(listOfFields)
 
 
 if __name__ == '__main__':
